b6012fc0ea4f092305db15da73e5f4f183015838
Dylan Hanson <cdh.create@gmail.com>
2021-04-14 17:26:32 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
   db_path: './users.db',
-  allowed_domains: ['ucla.edu'],
+  allowed_domains: ['vanderbilt.edu'],
   discord: {
     server_id: '754931521315536937',
     verified_role_name: 'Verified',
   },
   sendgrid: {
     template_id: 'd-f2190ba3825945a79e24cf06b2fd984c',
-    sender: 'rnema@ucla.edu',
+    sender: 'charles.d.hanson@vanderbilt.edu',
   },
 };
